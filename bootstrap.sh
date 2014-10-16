099f5c54bbd434e3b20a8a5043ea8d6f90706715
manan <nxmanan@gmail.com>
2014-10-16 11:56:09 +0800

    增加oh-my-zsh和prelude的安装脚本

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 #建立各项配置文件的软链，请做好相应文件的备份！
 
+#shell设置
+#安装oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
+
 #链接shell文件
 ln -sf ~/.dotfiles/dotshells/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/dotshells/.bash_profile ~/.bash_profile
 
 rm -rf ~/.oh-my-zsh/custom
 ln -sf ~/.dotfiles/dotshells/oh-my-zsh/custom ~/.oh-my-zsh/custom
+
+chsh -s /bin/zsh
+source ~/.zshrc
+source ~/.bash_profile
+
+#emacs设置
+#安装emacs配置prelude
+curl -L http://git.io/epre | sh
 
 #链接emacs文件
 ln -sf ~/.dotfiles/emacs/prelude/prelude-modules.el ~/.emacs.d/prelude-modules.el
