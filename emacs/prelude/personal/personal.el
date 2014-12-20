b309cce9a65e96b313dfdceaac3bbbc8475ef4c6
manan <nxmanan@gmail.com>
2014-12-20 10:29:13 +0800

    update ditaa

--- emacs/prelude/personal/personal.el
+++ emacs/prelude/personal/personal.el
@@ -40,5 +40,43 @@
           (require 'org-pm)))
 (setq org-pm-project-data-file-path "~/.emacs.d/personal/savefile/org-pm-project-data.el")
 
+;;picture export
+(setq org-ditaa-jar-path "~/.emacs.d/personal/ditaa0_9.jar")
+;;(setq org-plantuml-jar-path "~/java/plantuml.jar")
+
+(add-hook 'org-babel-after-execute-hook 'bh/display-inline-images 'append)
+
+; Make babel results blocks lowercase
+(setq org-babel-results-keyword "results")
+
+(defun bh/display-inline-images ()
+  (condition-case nil
+      (org-display-inline-images)
+    (error nil)))
+
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (dot . t)
+         (ditaa . t)
+         (R . t)
+         (python . t)
+         (ruby . t)
+         (gnuplot . t)
+         (clojure . t)
+         (sh . t)
+         (ledger . t)
+         (org . t)
+         (plantuml . t)
+         (latex . t))))
+
+; Do not prompt to confirm evaluation
+; This may be dangerous - make sure you understand the consequences
+; of setting this -- see the docstring for details
+(setq org-confirm-babel-evaluate nil)
+
+; Use fundamental mode when editing plantuml blocks with C-c '
+;;(add-to-list 'org-src-lang-modes (quote ("plantuml" . fundamental)))
+
 (provide 'personal)
 ;;; personal ends here
